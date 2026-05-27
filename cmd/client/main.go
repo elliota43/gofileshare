2c6c20e4e8892c187c483d29bc96ebb5762e6347
Elliot Anderson <elliot.anderson43@yahoo.com>
2026-05-27 16:53:40 -0400

    fixing duplicated client code

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,15 +2,51 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"os"
 
-	"github.com/elliota43/wormbeam/internal/relay"
+	"github.com/elliota43/wormbeam/internal/client"
 )
 
+const defaultRelay = "18.191.117.21:8080"
+
 func main() {
-	addr := flag.String("addr", ":8080", "listen address")
+	relayAddr := flag.String("relay", defaultRelay, "relay server address")
+	flag.Usage = usage
 	flag.Parse()
 
-	s := relay.New(nil)
-	log.Fatal(s.ListenAndServe(*addr))
+	args := flag.Args()
+	if len(args) < 1 {
+		usage()
+		os.Exit(2)
+	}
+
+	c, err := client.Dial(*relayAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer c.Close()
+
+	switch args[0] {
+	case "host":
+		err = client.Host(c)
+	case "join":
+		if len(args) < 2 {
+			log.Fatalf("usage: %s join <code>", os.Args[0])
+		}
+		err = client.Join(c, args[1])
+	default:
+		usage()
+		os.Exit(2)
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "usage: %s [-relay addr] host\n", os.Args[0])
+	fmt.Fprintf(os.Stderr, "       %s [-relay addr] join <code>\n", os.Args[0])
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,54 +2,15 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
-	"os"
 
-	"github.com/elliota43/wormbeam/internal/client"
+	"github.com/elliota43/wormbeam/internal/relay"
 )
 
-const defaultRelay = "16.59.53.129:8080"
-
 func main() {
-	relayAddr := flag.String("relay", defaultRelay, "relay server address")
-	flag.Usage = usage
+	addr := flag.String("addr", ":8080", "listen address")
 	flag.Parse()
 
-	args := flag.Args()
-	if len(args) < 1 {
-		usage()
-		os.Exit(2)
-	}
-
-	c, err := client.Dial(*relayAddr)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer c.Close()
-
-	switch args[0] {
-	case "host":
-		err = client.Host(c)
-
-	case "join":
-		if len(args) < 2 {
-			log.Fatalf("usage: %s join <code>", os.Args[0])
-		}
-		err = client.Join(c, args[1])
-
-	default:
-		usage()
-		os.Exit(2)
-	}
-
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func usage() {
-	fmt.Fprintf(os.Stderr, "usage: %s [-relay addr] host\n", os.Args[0])
-	fmt.Fprintf(os.Stderr, "       %s [-relay addr] join <code>\n", os.Args[0])
+	s := relay.New(nil)
+	log.Fatal(s.ListenAndServe(*addr))
 }
